845441cbfe53d481f71976b5fc9dc9a1c37b1db4
jaime <jaimecvr96@gmail.com>
2022-12-06 20:07:21 +0100

    appcontroller select/insert

--- src/main/kotlin/controlador/AppController.kt
+++ src/main/kotlin/controlador/AppController.kt
@@ -1,129 +1,120 @@
 package controlador
 
 import aplicacion.modelo.GestorModelo
+import modelo.clases.Producto
+import modelo.clases.Proveedor
 import modelo.sentencias.SentenciasSQL
 import vista.Vista
 import java.sql.Connection
+import java.sql.ResultSet
 import java.sql.SQLException
 
 class AppController(val vista: Vista) {
 
     private val gestor = GestorModelo.getInstance()
-    fun selectAll(){
-        try{
+    fun selectProducto(query: String) {
+        try {
 
             val cn: Connection = gestor.conect()
-            val ps = cn.prepareStatement("select * from empleados")
-            val rs = ps.executeQuery()
-            while(rs.next()){
+            val ps = cn.prepareStatement(query)
+            val rs: ResultSet = ps.executeQuery()
+            while (rs.next()) {
                 println(rs.getString(1))
                 println(rs.getString(2))
-                println(rs.getString(3))
-                println(rs.getString(4))
+                println(rs.getInt(3))
+                println(rs.getDouble(4))
+                println(rs.getString(5))
             }
-        }catch(s: SQLException){
+        } catch (s: SQLException) {
             s.printStackTrace()
-        }
-        finally {
+        } finally {
             gestor.disconect()
         }
     }
 
 
-
-
-    fun select(){
+    fun selectProveedor() {
         try {
             val cn: Connection = gestor.conect()
             val ps =
-                cn.prepareStatement(SentenciasSQL)
+                cn.prepareStatement(SentenciasSQL.selectAllProducto)
             val rs = ps.executeQuery()
             while (rs.next()) {
                 println(rs.getString(1))
                 println(rs.getString(2))
                 println(rs.getString(3))
-                println(rs.getString(4))
+                println(rs.getInt(4))
+                println(rs.getString(5))
             }
 
         } catch (s: SQLException) {
             s.printStackTrace()
-        }finally {
+        } finally {
             gestor.disconect()
         }
 
     }
 
-    fun insert(){
+    fun insertProducto(query: String, producto: Producto) {
         try {
-            gestor.cargarDriver()
-            val cn: Connection = gestor.establecerConexion()!!
-            val ps = cn.prepareStatement()
-            ps.setString(1, empleado.id.lowercase())
-            ps.setString(2, empleado.nombre)
-            ps.setString(3, empleado.oficio)
-            ps.setString(4, empleado.departamento)
+
+            val cn: Connection = gestor.conect()
+            val ps = cn.prepareStatement(query)
+            ps.setString(1, producto.id)
+            ps.setString(2, producto.nombre)
+            ps.setInt(3, producto.existencias)
+            ps.setDouble(4, producto.precio)
+            ps.setString(5, producto.tipo)
+
             ps.executeUpdate()
 
         } catch (s: SQLException) {
             s.printStackTrace()
-        }finally {
-            gestor.cerrarConexion()
+        } finally {
+            gestor.disconect()
         }
     }
 
-    fun delete(id: String){
+    fun insertProveedor(query: String, proveedor: Proveedor) {
         try {
-            gestor.cargarDriver()
-            val cn: Connection = gestor.establecerConexion()!!
-            val ps =
-                cn.prepareStatement("delete from empleados where id = '" + id.lowercase() + "'")
+
+            val cn: Connection = gestor.conect()
+            val ps = cn.prepareStatement(query)
+            ps.setString(1, proveedor.id)
+            ps.setString(2, proveedor.nombre)
+            ps.setString(3, proveedor.direccion)
+            ps.setInt(4, proveedor.telefono)
+            ps.setString(5, proveedor.idProd)
+
             ps.executeUpdate()
+
         } catch (s: SQLException) {
             s.printStackTrace()
-        }finally {
-            gestor.cerrarConexion()
-        }
-    }
-
-    fun update(){
-
-        try {
-            gestor.cargarDriver()
-            val cn: Connection = gestor.establecerConexion()!!
-            val ps =
-                cn.prepareStatement("update empleados set " + columna + "= ? where id = '" + id.lowercase() + "'")
-            ps.setString(1, valor)
-            ps.executeUpdate()
-        } catch (s: SQLException) {
-            s.printStackTrace()
-        }finally {
-            gestor.cerrarConexion()
+        } finally {
+            gestor.disconect()
         }
     }
 
 
-
-
-    fun onAllProducts(){
+    /*fun onAllProducts() {
         val gestor: GestorModelo = GestorModelo.getInstance()
         gestor.conect()
-        val listaProductos:List<Producto>?
 
-        if(listaProductos == null){
+        if (listaProductos == null) {
             vista.basedeDatosCaida()
-        }else if(listaProductos.size == 0){
+        } else if (listaProductos.size == 0) {
             vista.noProdStock()
-        }else if(listaProductos.size>0){
-            for(producto in listaProductos){
+        } else if (listaProductos.size > 0) {
+            for (producto in listaProductos) {
                 vista.showProduct(producto)
             }
-        }else{
+        } else {
             vista.errorGeneral()
         }
-    }
+    }*/
 
     fun onStart(): Int {
-        val opcion  = vista.mainMenu()
+        val opcion = vista.mainMenu()
         return opcion
     }
 
@@ -132,4 +123,5 @@ class AppController(val vista: Vista) {
         gestor.disconect()
         vista.salir()
     }
+}
 
--- src/main/kotlin/modelo/sentencias/SentenciasSQL.kt
+++ src/main/kotlin/modelo/sentencias/SentenciasSQL.kt
@@ -1,6 +1,6 @@
 package modelo.sentencias
 
-class SentenciasSQL {
+ object SentenciasSQL {
     val selectAllProducto:String = "SELECT * FROM producto"
     val insertProduct:String = "INSERT INTO producto VALUES(?,?,?,?,?)"
     val deleteProdForId:String = "DELETE FROM producto WHERE id=?"
